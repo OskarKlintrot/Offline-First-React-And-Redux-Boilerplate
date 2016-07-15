73e6cb2f3299b287ecbfef8bd68c4b13d8e21a62
Oskar Klintrot <oskar.klintrot@gmail.com>
2016-07-15 13:08:28 +0200

    Use Object.freeze()

--- src/app/redux/actions/actionTypes.js
+++ src/app/redux/actions/actionTypes.js
@@ -1,14 +1,14 @@
-export const ActionTypesApp = {
+export const ActionTypesApp = Object.freeze({
   RESET: 'RESET',
   SET_USER: 'SET_USER',
   // You can have several objects here...
-}
+})
 
-export const ActionTypesSettings = {
+export const ActionTypesSettings = Object.freeze({
   RESET: 'RESET',
   // ...just to increase code readability
-}
+})
 
 const ActionTypes = Object.assign({}, ActionTypesApp, ActionTypesSettings)
 
-export default ActionTypes
+export default Object.freeze(ActionTypes)
